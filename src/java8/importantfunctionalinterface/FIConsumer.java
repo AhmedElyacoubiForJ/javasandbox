1ecc8fc762e4fc7c834b7d59ae96e926071070cd
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-06-05 01:24:37 +0200

    Consumer Interface demo

--- src/java8/importantfunctionalinterface/FIConsumer.java
+++ src/java8/importantfunctionalinterface/FIConsumer.java
@@ -1,4 +1,19 @@
 package java8.importantfunctionalinterface;
 
+import java.util.Arrays;
+import java.util.function.Consumer;
+
 public class FIConsumer {
+    public static void main(String[] args) {
+        Consumer<String> normalPrinter = System.out::println;
+        Consumer<String> UpperCasePrinter = s -> System.out.println(s.toUpperCase());
+        Consumer<String> normalPrinterThenUpperCasePrinter =
+                normalPrinter.andThen(UpperCasePrinter);
+        //normalPrinterThenUpperCasePrinter.accept("Hello World");
+
+        //
+        Arrays.asList("Hello", "World")
+                .stream()
+                .forEach(normalPrinterThenUpperCasePrinter);
+    }
 }
