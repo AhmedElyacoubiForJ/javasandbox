cae56d608db3c62e9d4503ab793843e3b01dfc81
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-06-05 02:50:17 +0200

    demo class to appropriate package moved

--- src/java8/streams/ForEachAndPeek.java
+++ src/java8/streams/ForEachAndPeek.java
@@ -1,8 +1,7 @@
-package java8.importantfunctionalinterface;
+package java8.streams;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ForEachAndPeek {
