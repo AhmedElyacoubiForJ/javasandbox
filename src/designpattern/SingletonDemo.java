74223437bcac0477aeb18f3edb553d95933e13e0
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-10-11 17:06:23 +0200

    refact

--- src/designpattern/SingletonDemo.java
+++ src/designpattern/SingletonDemo.java
@@ -1,2 +1,37 @@
-package designpattern;public class SingeltonDemo {
+package designpattern;
+
+/*
+ * Stellt sicher, dass eine Klasse nur eine Instanz hat und bietet einen globalen Zugriffspunkt darauf.
+ * In einem Logging-System wird oft das Singleton-Muster verwendet, um sicherzustellen,
+ * dass nur ein Logger existiert, der von verschiedenen Teilen der Anwendung verwendet werden.
+ * */
+public class SingletonDemo {
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("First log message");
+        logger2.log("Second log message");
+
+        System.out.println(logger1 == logger2); // true
+    }
+}
+
+class Logger {
+    private static Logger instance;
+
+    private Logger() {
+        // Private constructor to prevent instantiation from outside this class
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("Logger: " + message);
+    }
 }
